236685e0267e028bef830bd8daf60a2146c548e5
Kulikalov <kulikalovdenis@gmail.com>
2024-05-15 15:19:31 +0300

    added files

---
+++ main.js
@@ -0,0 +1,13 @@
+document.getElementById('changeBackground').addEventListener('click', function() {
+    var newColor = getRandomColor();
+    document.body.style.backgroundColor = newColor;
+});
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
